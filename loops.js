d5d2aec4eae84a4e73fc91414746019520ed9ac0
frankcao@me.com Cao <frankcao@me.com>
2018-06-19 11:19:27 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,16 @@
+var array = [];
+
+function forLoop(array) {
+for (var i = 0; i < 25; i++) {
+  array.push("Frank")
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while ( n > 0 ) {
+    for (i = n; i > 0; i--) {
+    console.log("done");
+    }
+  }
+}
